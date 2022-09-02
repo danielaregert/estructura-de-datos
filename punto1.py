278b04ab97b9025c2c91756364d8e189df23ab81
Daniela Regert <danielaregert@gmail.com>
2022-09-02 17:52:13 -0300

    NEW: punto1, ejercicio2

---
+++ punto1.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        Ejercicio2, punto1
+# Purpose:
+#
+# Author:      Daniela Regert
+#
+# Created:     02/09/2022
+# Copyright:   (c) Daniela 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def punto1():
+    num1= 80; num2 = 120
+    print(num1 + num2)
+
+if __name__ == '__main__':
+    punto1()
