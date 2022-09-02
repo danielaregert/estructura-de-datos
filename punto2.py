441f8126650638ad933e0605c4c1a35d69babbb2
Daniela Regert <danielaregert@gmail.com>
2022-09-02 17:53:48 -0300

    ejercicio2, punto2

---
+++ punto2.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        punto2
+# Purpose:
+#
+# Author:      Daniela
+#
+# Created:     02/09/2022
+# Copyright:   (c) Daniela 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def punto2():
+    cadena1 = "Hola, que tal?"; cadena2 = "Me llamo Daniela"
+    print (cadena1 + " " + cadena2)
+
+if __name__ == '__main__':
+    punto2()
