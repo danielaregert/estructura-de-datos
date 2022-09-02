d8e24c9b167d408bf531f1fdad74b36366707fbb
Daniela Regert <danielaregert@gmail.com>
2022-09-02 17:55:05 -0300

    ejercicio2, punto3

---
+++ punto3.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        ejercicio2, punto3
+# Purpose:
+#
+# Author:      Daniela Regert
+#
+# Created:     02/09/2022
+# Copyright:   (c) Daniela 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def ejercicio3():
+    nombre = input("Ingrese su nombre: ")
+    dni = input("Ingrese su dni: ")
+
+    print(f'Su nombre es {nombre}')
+    print(f'Su DNI es {dni}')
+
+if __name__ == '__main__':
+    ejercicio3()
