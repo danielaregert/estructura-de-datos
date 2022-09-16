7451cde7e38061a72ae1ad440cb1e6a2f4bffac0
Daniela Regert <danielaregert@gmail.com>
2022-09-16 15:47:40 -0300

    Ejercicio1 de funciones

---
+++ funciones/ejercicios/ejercicio1.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Daniela
+#
+# Created:     13/09/2022
+# Copyright:   (c) Daniela 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+def area_rectangulo(base, altura):
+        return base * altura
+
+
+if __name__ == '__main__':
+    area_rectangulo(2,3)
