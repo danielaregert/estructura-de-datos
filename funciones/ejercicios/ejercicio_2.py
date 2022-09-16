f7dff6aa6186e922fa5b5d386ffd915606833c7a
Daniela Regert <danielaregert@gmail.com>
2022-09-16 15:48:01 -0300

    ejercicio2 funciones

---
+++ funciones/ejercicios/ejercicio_2.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Daniela
+#
+# Created:     13/09/2022
+# Copyright:   (c) Daniela 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+
+def area_circulo(radio):
+    area = 3.14159 * radio ** 2
+    return area
+
+
+if __name__ == '__main__':
+    area_circulo(20)
