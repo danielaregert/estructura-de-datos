a478a1a14eba8476a728a5f746f90966a8110250
Daniela Regert <danielaregert@gmail.com>
2022-09-16 15:48:43 -0300

    ejercicio 4

---
+++ funciones/ejercicios/ejercicio_4.py
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Daniela
+#
+# Created:     13/09/2022
+# Copyright:   (c) Daniela 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def intermedio(numero1, numero2):
+    return (numero1 + numero2) / 2
+
+if __name__ == '__main__':
+    intermedio(1,2)
